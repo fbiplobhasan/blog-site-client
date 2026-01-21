091d57694bc182d8094dc888827aed5241bff13a
Biplob Hasan <your-email@example.com>
2026-01-21 10:15:19 +0600

    add sidebar component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@
 export default async function AboutPage() {
   await new Promise((resolve) => setTimeout(resolve, 4000));
 
-  throw new Error("Something Went Wrong.")
+  // throw new Error("Something Went Wrong.")
 
   return <div>about page</div>;
 }
