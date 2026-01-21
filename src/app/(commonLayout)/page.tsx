dd6ded115c84c8110c60fa08275ced4ab026e6b6
Biplob Hasan <your-email@example.com>
2026-01-21 14:58:44 +0600

    add server side fetch and retrieved session data and set cache no store

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,18 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const session = await authClient.getSession();
+  const cookieStore = await cookies();
+
+  console.log(cookieStore.get("better-auth.session_token"));
+
+  const res = await fetch("http://localhost:5001/api/auth/get-session", {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+  const session = await res.json();
   console.log(session);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
