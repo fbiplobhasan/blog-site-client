57fa9ecc409c8757764d7829d8d6b6ab2ca4ccbe
Biplob Hasan <your-email@example.com>
2026-01-21 16:12:54 +0600

    add env and zod validation

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,19 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { cookies } from "next/headers";
+import { userService } from "@/services/user.service";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-
-  console.log(cookieStore.get("better-auth.session_token"));
-
-  const res = await fetch("http://localhost:5001/api/auth/get-session", {
-    headers: {
-      Cookie: cookieStore.toString(),
-    },
-    cache: "no-store",
-  });
-  const session = await res.json();
-  console.log(session);
+  const { data } = await userService.getSession();
+  console.log(data);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Button variant={"outline"}>Click Here</Button>
---
+++ src/env.ts
@@ -0,0 +1,26 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    BACKEND_URL: z.url(),
+    FRONTEND_URL: z.url(),
+    AUTH_URL: z.url(),
+    API_URL: z.url(),
+  },
+
+  //   client
+  client: {
+    NEXT_PUBLIC_TEST_CLIENT: z.string(),
+  },
+
+  runtimeEnv: {
+    BACKEND_URL: process.env.BACKEND_URL,
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    AUTH_URL: process.env.AUTH_URL,
+    API_URL: process.env.API_URL,
+    NEXT_PUBLIC_TEST_CLIENT: process.env.NEXT_PUBLIC_TEST_CLIENT,
+  },
+});
+
+// jodi project run korar shathe-shathe meaningfull error chai next.config.ts a import kore rakhte hobe.
