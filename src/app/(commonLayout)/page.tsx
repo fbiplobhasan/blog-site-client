2d11495394427753d2741426645d9cbe095360e8
Biplob Hasan <your-email@example.com>
2026-01-21 22:08:27 +0600

    add get post implementation

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { userService } from "@/services/user.service";
+import { blogService } from "@/services/blog.service";
 
 export default async function Home() {
-  const { data } = await userService.getSession();
+  const { data } = await blogService.getBlogPost();
   console.log(data);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
