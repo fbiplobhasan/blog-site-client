41c5b29a89ff03d02c8dd53948000bd3713ae817
Biplob Hasan <your-email@example.com>
2026-01-24 23:04:34 +0600

    add dynamic role control and redirect create blog page

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,22 +1,15 @@
 import { AppSidebar } from "@/components/layout/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { Roles } from "@/constants/roles";
+import { userService } from "@/services/user.service";
 import { ReactNode } from "react";
 
-export default function Page({
-  children,
+export default async function DashboardLayout({
   admin,
   user,
 }: {
@@ -24,9 +17,8 @@ export default function Page({
   admin: ReactNode;
   user: ReactNode;
 }) {
-  const userInfo = {
-    role: "user",
-  };
+  const { data } = await userService.getSession();
+  const userInfo = data.user;
 
   return (
     <SidebarProvider>
@@ -38,22 +30,9 @@ export default function Page({
             orientation="vertical"
             className="mr-2 data-[orientation=vertical]:h-4"
           />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          {userInfo.role === "admin" ? admin : user}
+          {userInfo.role === Roles.admin ? admin : user}
         </div>
       </SidebarInset>
     </SidebarProvider>
